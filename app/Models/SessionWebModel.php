e2735ba916d9151555bfe1cac9422af085086ee2
Frederick-dev23 <79848521+Frederick-dev23@users.noreply.github.com>
2021-03-04 08:19:34 -0400

    add model

--- app/Models/SessionWebModel.php
+++
@@ -1,8 +0,0 @@
-<?php namespace App\Models;
- 
-use CodeIgniter\Model;
- 
-class SessionWebModel extends Model{
-    protected $table = 'sessionweb';
-    protected $allowedFields = ['id_session','email','nom','prenom','userCreatedAt'];
-}
